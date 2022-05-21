72b85a559a1549529a506e68e3ce0b5708a73f40
Michael Prokopev <matroskin119@gmail.com>
2022-05-22 02:04:42 +0300

    third commit

--- script.js
+++ script.js
@@ -1,150 +1,276 @@
 $("#toggle").on('click', function () {
     if ( $(this).is(':checked') ) {
-        document.querySelector(".form-group-2 label").innerHTML = "Снять правило SPT";
+        document.querySelector(".form-group-2 label").innerHTML = "Снять правило LUKR";
         sortTable(".sortable");
     }
     else {
-        document.querySelector(".form-group-2 label").innerHTML = "Установить правило SPT";
+        document.querySelector(".form-group-2 label").innerHTML = "Установить правило LUKR";
         sortTable();
     }
 })
   
-
  getRandom = (min, max, num) => {
     return  Math.floor(Math.floor(Math.random() * (max - min + 1) + min) / num) * num;
 }
 
+// const rebuildTable = (t) => {
+//   for(let i = 1; i < numberOfdetails; i++){
+//     for(let j = 0; j < i; j++ ){
+//       if(parseInt(t.rows[j].cells[1].innerHTML) > parseInt(t.rows[j].cells[1].innerHTML)){
+//         let temp = t.rows[i];
+//         t.rows[i] = t.rows[i + 1];
+//         t.rows[i + 1] = temp;
+//       }
+//     }
+//     console.log(t.rows[i]);
+//   }
+//   return t;
+// }
+// const putNums = (index) => {
+//       return `<td class="detail_row">${parseInt(index + 1)}</td>`
+//   }
 
+//   const putStanki = (n) => {
 
-var numberOfdetails=document.getElementById("numberOfdetails").value;
-
-
-const table = document.getElementById("inputTable");
-
-//10 станков...
-createTable = () => {   
-    let table=document.getElementById("inputTable");
-    var numberOfdetails=document.getElementById("numberOfdetails").value;
-    
-    for(let i = 0; i < numberOfdetails; i++){
-        
-        var firstRow = table.insertRow(-1);
-        let cell1 = firstRow.insertCell(-1);
-        
-        for(let j = 0; j < 10; j++){
-           // var secondRow = table.insertRow(-1);
-            var cell2 = firstRow.insertCell(-1);
-            cell2.innerHTML = getRandom(0, 700, 50); 
-        }
+//         for(let i = 0; i < 10; i++){
+//          return `<td class="detail_row">${getRandom(0,700,50)}</td>`
+//         }
       
-       
-        cell1.innerHTML=i+1;
-    }
-   // var new_tab = new Tabulator("#inputTable", {});
+//     }
+ 
 
-   GetCellValues(numberOfdetails);
+// var data = [
+//   {value: [200,400,300,100,50,250,550,500,200,350]},
+//   {value: [50,450,300,100,50,250,550,500,0,350]},
+//   {value: [150,350,0,100,50,150,650,450,200,350]}
+// ];
+
+
+
+
+
+ 
+ createTable = () => {
+  var numberOfdetails = document.getElementById("numberOfdetails").value;
+
+  rebuildMatrix = (arr) => {
+  
+    arr.sort((a,b) => {
+      return a[0] - b[0];
+    })
+     return arr;
+   }
+
+  createMatrix = (stanki, numberOfdetails) => {
+    var matrix = [];
+    var dig_matrix = [];
+
+    for (let i = 0; i < numberOfdetails; i++) {
+        matrix[i] = [];
+        dig_matrix[i] = [];
+        for (let j = 0; j < stanki; j++) {
+           // var div = document.createElement('div');
+          //  div.className = 'cell';
+           // matrix[i][j] = div;
+            dig_matrix[i][j] = getRandom(0,700,50);
+
+           // div.innerHTML = getRandom(0,700,50);
+        }
+    }
+    
+    return dig_matrix;
+ }
+
+ var m = createMatrix(10,numberOfdetails);
+ console.log(m)
+
+  // var w =10;
+  // var h = numberOfdetails;
+  // var m = createMatrix(w,h);
+  
+  // var d = document.getElementsByClassName("container")[0];
+  // for(let i=0; i<m.length; i++){
+  //     let row = m[i]
+  //     for(let j=0; j<row.length; j++){
+  //         d.appendChild(row[j]);
+  //     }
+  //     d.appendChild(document.createElement('div')); //для разделения строк
+  // }
+
+//console.log(createMatrix(10, numberOfdetails));
+
+
+$("#toggle_spt").on('click', function () {
+  if ( $(this).is(':checked') ) {
+      document.querySelector(".form-group-3 label").innerHTML = "Снять правило LUKR";
+      console.log('/////////////////////ПОСЛЕ СОРТИРОВКИ///////////////////////////////////////////');
+      console.log(rebuildMatrix(m));
+  }
+  else {
+      document.querySelector(".form-group-3 label").innerHTML = "Установить правило LUKR";
+  }
+})
+
+
+
+
 }
 
 
 
-function sortTable(selector, compFunc) {
-    var mySelector = '.sortable';
-    var myCompFunc = function($td1, $td2, isAsc) {
-      var v1 = $.trim($td1.text()).replace(/,|\s+|%/g, '');
-      var v2 = $.trim($td2.text()).replace(/,|\s+|%/g, '');
-      var pattern = /^\d+(\.\d*)?$/;
-      if (pattern.test(v1) && pattern.test(v2)) {
-        v1 = parseFloat(v1);
-        v2 = parseFloat(v2);
-      }
-
-      return isAsc ? v1 > v2 : v1 < v2;
-    };
-
-    var doSort = function($tbody, index, compFunc, isAsc)
-    {
-      var $trList = $tbody.find("tr");
-      var len = $trList.length;
-      for(var i=0; i<len-1; i++) {
-        for(var j=0; j<len-i-1; j++) {
-          var $td1 = $trList.eq(j).find("td").eq(index);
-          var $td2 = $trList.eq(j+1).find("td").eq(index);
-
-          if (compFunc($td1, $td2, isAsc)) {
-            var t = $trList.eq(j+1);
-            $trList.eq(j).insertAfter(t);
-            $trList = $tbody.find("tr");
-          }
-        }
-      }
-      console.log(table.rows[1]);
-    }
-
-    var init = function() {
-      var $th = $("th" + selector);
-      this.$table = $th.closest("table");
-      var that = this;
-      $th.click(function(){
-        var index = $(this).index();
-        var asc = $(this).attr('data-asc');
-        isAsc = asc === undefined ? true : (asc > 0 ? true : false);
-
-        doSort(that.$table.find("tbody"), index, compFunc, isAsc);
-
-        $(this).attr('data-asc', 1 - (isAsc ? 1 : 0));
-      });
-
-      $th.css({'cursor': 'pointer'})
-         .attr('title', "Нажмите, чтобы отсортировать")
-         .append('&nbsp;<i class="fa fa-long-arrow-down" style="color:#2196F3" aria-hidden="true"></i><i class="fa fa-long-arrow-up" style="color:#2196F3" aria-hidden="true"></i>');
-    };
-
-
-    selector = selector || mySelector;
-    compFunc = compFunc || myCompFunc;
-
-    init();
-  }
 
 
 
- GetCellValues = (numberOfdetails) => {
+
+
+//10 станков...
+// createTable = () => {   
+
+//   // добавим саму таблицу на страницу
+//   $('.container').append('<table class="dataTable"><thead></thead><tbody></tbody></table>'); 
   
-    let defaultArray = [];
+//   // добавим шапку
+//   $('.dataTable > thead').append(
+//       '<tr><th>№</th></tr>'
+//     );
+  
+//   // пройдем уиклом по всем элементам массива и сгенерируем строки таблицы
+//   data.forEach((item, i, arr) => {
+//     $('.dataTable > tbody').append(
+//       `<tr class="row-item">
+//       <td>${(i + 1)}</td>
+//       ${item.value.map(el => {
+//         return `<td class="time">${el}</td>`
+//       })}`
+//     );
+//   });
 
-    /*---------------------------------------------MY EDIT----------------------------------------------------------------*/
 
-    var table = document.getElementById('inputTable');
-    let tempSum;
+//   const compareRows = (table) => {
+//     var rows = table.rows;
+//     var cell, cells;
+//     var rowText;
+  
+//     // For each row in the table
+//     for (var i=0, iLen=rows.length; i<iLen; i++) {
+//       row = rows[i];
+//       cells = row.cells;
+//       // Compare the text in each cell
+//       for (var j=0; j < 1; j++) {
+//       }
+//       var arr = [];
+      
+//     }
+ 
+
+//   }
+  
+// }
+
+
+
+
+
+//LUKR, MWKR
+
+// function sortTable(selector, compFunc) {
+//     var mySelector = '.sortable';
+//     var myCompFunc = function($td1, $td2, isAsc) {
+//       var v1 = $.trim($td1.text()).replace(/,|\s+|%/g, '');
+//       var v2 = $.trim($td2.text()).replace(/,|\s+|%/g, '');
+//       var pattern = /^\d+(\.\d*)?$/;
+//       if (pattern.test(v1) && pattern.test(v2)) {
+//         v1 = parseFloat(v1);
+//         v2 = parseFloat(v2);
+//       }
+
+//       return isAsc ? v1 > v2 : v1 < v2;
+//     };
+
+//     var doSort = function($tbody, index, compFunc, isAsc)
+//     {
+//       var $trList = $tbody.find("tr");
+//       var len = $trList.length;
+//       for(var i=0; i<len-1; i++) {
+//         for(var j=0; j<len-i-1; j++) {
+//           var $td1 = $trList.eq(j).find("td").eq(index);
+//           var $td2 = $trList.eq(j+1).find("td").eq(index);
+
+//           if (compFunc($td1, $td2, isAsc)) {
+//             var t = $trList.eq(j+1);
+//             $trList.eq(j).insertAfter(t);
+//             $trList = $tbody.find("tr");
+//           }
+//         }
+//       }
+//       console.log(table.rows[1]);
+//     }
+
+//     var init = function() {
+//       var $th = $("th" + selector);
+//       this.$table = $th.closest("table");
+//       var that = this;
+//       $th.click(function(){
+//         var index = $(this).index();
+//         var asc = $(this).attr('data-asc');
+//         isAsc = asc === undefined ? true : (asc > 0 ? true : false);
+
+//         doSort(that.$table.find("tbody"), index, compFunc, isAsc);
+
+//         $(this).attr('data-asc', 1 - (isAsc ? 1 : 0));
+//       });
+
+//       $th.css({'cursor': 'pointer'})
+//          .attr('title', "Нажмите, чтобы отсортировать")
+//          .append('&nbsp;<i class="fa fa-long-arrow-down" style="color:#2196F3" aria-hidden="true"></i><i class="fa fa-long-arrow-up" style="color:#2196F3" aria-hidden="true"></i>');
+//     };
+
+
+//     selector = selector || mySelector;
+//     compFunc = compFunc || myCompFunc;
+
+//     init();
+//   }
+
+
+
+//  GetCellValues = (numberOfdetails) => {
+  
+//     let defaultArray = [];
+
+//     /*---------------------------------------------MY EDIT----------------------------------------------------------------*/
+
+//     var table = document.getElementById('inputTable');
+//     let tempSum;
    
 
-    for (var r = 1, n = table.rows.length; r < n; r++) {
-        tempSum = 0;
-        for (var c = 1, m = table.rows[r].cells.length; c < m; c++) {
-         tempSum += parseInt(table.rows[r].cells[c].innerHTML);
+//     for (var r = 1, n = table.rows.length; r < n; r++) {
+//         tempSum = 0;
+//         for (var c = 1, m = table.rows[r].cells.length; c < m; c++) {
+//          tempSum += parseInt(table.rows[r].cells[c].innerHTML);
         
-        }
-        defaultArray.push(tempSum);
-        continue;
-    }
+//         }
+//         defaultArray.push(tempSum);
+//         continue;
+//     }
 
 
-    let filteredArray = defaultArray.filter(el => el !== 0);
+//     let filteredArray = defaultArray.filter(el => el !== 0);
 
-    let sortedTable = document.getElementById('sortedTable');
+//     let sortedTable = document.getElementById('sortedTable');
 
-    for(let i = 0; i < numberOfdetails; i++){
-        let firstRow = sortedTable.insertRow(-1);
-        let cell1 = firstRow.insertCell(-1);
-        let cell2 = firstRow.insertCell(-1);
-        // for(let j = 0; j < 10; j++){
-        //     cell2 = firstRow.insertCell(-1);
+//     for(let i = 0; i < numberOfdetails; i++){
+//         let firstRow = sortedTable.insertRow(-1);
+//         let cell1 = firstRow.insertCell(-1);
+//         let cell2 = firstRow.insertCell(-1);
+//         // for(let j = 0; j < 10; j++){
+//         //     cell2 = firstRow.insertCell(-1);
             
-        // }
-        cell2.innerHTML = filteredArray[i];
-        cell1.innerHTML=i+1;
-    }
-  }
+//         // }
+//         cell2.innerHTML = filteredArray[i];
+//         cell1.innerHTML=i+1;
+//     }
+//   }
 
 
 // generateGanttChartData = (data) => {   
